44a37d37200b3f2e8a0bb85a18a29b41355bcfd5
nadezhda.porotkova <nadezhda.porotkova@karuna.group>
2022-12-15 11:57:59 +0100

    junit added

--- src/TestAuth.java
+++ src/TestAuth.java
@@ -1,22 +1,16 @@
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class TestAuth {
-
-    public static void main(String[] args) throws IOException {
-        Network test = new Network();
-        LocalesResponse locales = test.get("https://api.binomo.com/platform/locales", LocalesResponse.class, null);
-        Locales data = locales.data;
-        Assertions.assertEquals("en", data.defaultLocale);
-        Assertions.assertTrue(data.availableLocales.contains("ru"));
-        //testAuth();
-        testAuthErrors();
-    }
-
-    public static void testAuth() throws IOException{
+    
+    @Test
+    public void testAuth() throws IOException{
         Map<String, String> headers = new HashMap<>();
         headers.put("Device-Type",	"android");
         headers.put("Device-Id",	"1669807052602-1096312346268349946");
@@ -32,7 +26,8 @@ public class TestAuth {
         Assertions.assertEquals(149320504, response.data.id);
     }
 
-    public static void testAuthErrors() throws IOException{
+    @Test
+    public void testAuthErrors() throws IOException{
         Map<String, String> headers = new HashMap<>();
         headers.put("Device-Type",	"android");
         headers.put("Device-Id",	"1669807052602-1096312346268349946");
